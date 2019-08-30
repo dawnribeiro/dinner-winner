6aa9b636a43d4182e47ae10f9252ce8395de56af
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-08-30 15:56:06 -0400

    initial setup

---
+++ Controllers/Restaurant.cs
@@ -0,0 +1,10 @@
+namespace dinner_winner.Controllers
+{
+  public class Restaurant
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+
+  }
+}
---
+++ Migrations/20190830195413_AddedRestaurantTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace sdgreacttemplate.Migrations
+{
+    public partial class AddedRestaurantTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
