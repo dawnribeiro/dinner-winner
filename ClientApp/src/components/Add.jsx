d9cd1e13e5cbcea561f3b584c8629470f7e18e37
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-10-09 17:04:59 -0400

    added all restaurants to add page

--- ClientApp/src/components/Add.jsx
+++ ClientApp/src/components/Add.jsx
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import AllRestaurants from './AllRestaurants'
+import auth from '../Auth'
+// import AllRestaurants from './AllRestaurants'
 
 export default function Add() {
   const [restaurant, setRestaurant] = useState([])
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    if (!auth.isAuthenticated()) {
+      window.location.href = '/'
+    } else {
+      axios.get('api/restaurant').then(resp => {
+        setRestaurants(resp.data)
+        console.log(resp.data)
+      })
+    }
+  }, [])
+
+  const deleteRestaurant = r => {
+    let deletedRestaurant = r.id
+    axios.delete(`api/restaurant/${deletedRestaurant}`).then(resp => {
+      setRestaurants(oldRestaurants =>
+        oldRestaurants.filter(f => f.id !== r.id)
+      )
+    })
+  }
 
   const createRestaurant = e => {
     const data = { ...restaurant }
     axios.post('/api/Restaurant', data).then(resp => {
       console.log(resp.data)
     }, [])
-    e.preventDefault()
     e.target.reset()
   }
 
@@ -55,7 +76,18 @@ export default function Add() {
         <label></label>
         <button>Add</button>
       </form>
-      <AllRestaurants />
+      {/* <AllRestaurants /> */}
+      <ul>
+        {restaurants.map(r => {
+          return (
+            <li key={r.id}>
+              <h3>{r.name}</h3>
+              <p>{r.location}</p>
+              <button onClick={() => deleteRestaurant(r)}>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
     </section>
   )
 }
