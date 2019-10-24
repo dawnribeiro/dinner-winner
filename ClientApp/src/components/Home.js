e0d1e5d7c0930dc69f26bc93b48537911890d503
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-10-24 17:03:00 -0400

    created dropdown menu

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -5,6 +5,7 @@ import auth from '../Auth'
 export default function Home() {
   const [restaurants, setRestaurants] = useState([])
   const [randomRestaurant, setRandomRestaurant] = useState([])
+  const [locations, setLocations] = useState([])
 
   useEffect(() => {
     axios.get('api/restaurant').then(resp => {
@@ -30,10 +31,40 @@ export default function Home() {
   return (
     <section className="home-container">
       <h1>Winner Winner, Chicken Dinner!!</h1>
-      <button className="random-btn" onClick={() => getRandom(restaurants)}>
-        Dinner Time
-      </button>
-      <h2>{randomRestaurant.name}</h2>
+      <div className="btn-container">
+        <button className="random-btn" onClick={() => getRandom(restaurants)}>
+          All
+        </button>
+        <div className="dropdown">
+          <button className="dropbtn" onClick={() => getLocations(locations)}>
+            Location
+          </button>
+          <div className="dropdown-content">
+            <ul className="btn-list">
+              <li>
+                <button>1</button>
+              </li>
+            </ul>
+            {/* <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a> */}
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Type</button>
+          <div className="dropdown-content">
+            <ul className="btn-list">
+              <li>
+                <button>1</button>
+              </li>
+            </ul>
+            {/* <a href="#">Link 4</a>
+            <a href="#">Link 5</a>
+            <a href="#">Link 6</a> */}
+          </div>
+        </div>
+      </div>
+      <h2 className="random-restaurant">{randomRestaurant.name}</h2>
     </section>
   )
 }
